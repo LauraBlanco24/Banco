14006337098d72fe32fca2ad471aa418a46a7ee1
LauraBlanco24 <laura.blanco01@correo.usa.edu.co>
2020-08-27 17:14:28 -0500

    elimine metodo constructor

--- src/Cliente.java
+++ src/Cliente.java
@@ -4,10 +4,5 @@ public class Cliente {
     Cuenta cuenta;
     String tipoDocumento;
 
-    public Cliente(String nombre, int id, Cuenta cuenta, String tipoDocumento) {
-        this.nombre = nombre;
-        this.id = id;
-        this.cuenta = cuenta;
-        this.tipoDocumento = tipoDocumento;
-    }
+   
 }
