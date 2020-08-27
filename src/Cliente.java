c9586956cd351c2b1f576292c175f5a67cbc0fd5
LauraBlanco24 <laura.blanco01@correo.usa.edu.co>
2020-08-27 17:23:35 -0500

    Cree getter y setter

--- src/Cliente.java
+++ src/Cliente.java
@@ -4,5 +4,42 @@ public class Cliente {
     Cuenta cuenta;
     String tipoDocumento;
 
-   
+    public Cliente(String nombre, int id, Cuenta cuenta, String tipoDocumento) {
+        this.nombre = nombre;
+        this.id = id;
+        this.cuenta = cuenta;
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
 }
